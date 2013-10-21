abfc060cc9d77e88d4833d4ab4b7d9b97aec89c0
Amey Sakhadeo <me@ameyms.com>
2013-10-21 18:29:12 +0530

    Replace one last occurance of typeof number

--- jquery.flot.js
+++ jquery.flot.js
@@ -856,7 +856,7 @@ Licensed under the MIT license.
             var a = obj[coord + "axis"];
             if (typeof a == "object") // if we got a real axis, extract number
                 a = a.n;
-            if (typeof a != "number")
+            if (!$.isNumeric(a))
                 a = 1; // default to first axis
             return a;
         }
